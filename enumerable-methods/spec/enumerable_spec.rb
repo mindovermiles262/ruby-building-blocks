07810bcd3e5a4648597cf84408277ac79ce65620
Erik Johnson <mindovermiles262@gmail.com>
2017-03-23 07:34:52 -0700

    finish rspec

--- enumerable-methods/spec/enumerable_spec.rb
+++ enumerable-methods/spec/enumerable_spec.rb
@@ -36,10 +36,53 @@ describe Enumerable do
             expect(arr.my_all? { |i| i < 6 }).to eq(false)
         end
     end
-=begin
+
     describe "my_any?" do
         it "returns true if one condition is true" do
-            expect()
+            expect(arr.my_any? { |i| i > 6 }).to eq(true)
+        end
+        it "returns false if all conditions are false" do
+            expect(arr.my_any? { |i| i % 10 == 0 }).to eq(false)
+        end
     end
-=end
+
+    describe "my_none?" do
+        it "returns true if all conditions are false" do
+            expect(arr.my_none? { |i| i % 10 == 0 }).to eq(true)
+        end
+        it "returns false if one condition is true" do
+            expect(arr.my_none? { |i| i > 6 }).to eq(false)
+        end
+    end 
+
+    describe "my_count" do
+        it "returns the number of items in an array if no argument is passed" do
+            expect(arr.my_count).to eq(4)
+        end
+        it "returns the number of items equal to passed argument" do
+            expect(arr.my_count(2)).to eq(2)
+            expect(arr.my_count(4)).to eq(1)
+        end
+        it "counts number of true elements if block is given" do
+            expect(arr.my_count{ |i| i < 6 }).to eq(3)
+        end
+    end
+
+    describe "my_map" do
+        it "returns new array with the results from running block once" do
+            expect(arr.my_map{ |i| i**2 }).to eq([4, 16, 49, 4])
+        end
+        it "returns enumerator if no block is given" do
+            expect(arr.map{ "cat" }).to eq(["cat", "cat", "cat", "cat"])
+        end
+    end
+
+    describe "my_inject" do
+        it "uses a block and returns memo" do
+            expect(arr.my_inject{ |sum, i| sum + i }).to eq(15)
+        end
+        it "multiplies elements from array" do
+            expect(arr.my_inject(1){ |memo, n| memo * n }).to eq(112)
+        end
+    end
 end
